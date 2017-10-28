5b03bd1fe5ad6588d9f71ed46148544eb0716134
Alex Denton <alex.denton.dev@gmail.com>
2017-10-28 10:38:35 -0400

    Alternate implementation of problem 1

--- problem1/problem1.hs
+++ problem1/problem1.hs
@@ -1,8 +1,15 @@
 main = do
     print $ last' [1, 2, 3, 4]
     print $ last' ["first", "second", "last"]
+    print $ last'' [1, 2, 3]
 
 last' :: [a] -> a
-last' [] = error "Can not call 'last' on an empty list"
+last' [] = canNotCallLastOnEmptyList
 last' (x:[]) = x
 last' (x:xs) = last xs
+
+last'' :: [a] -> a
+last'' [] = canNotCallLastOnEmptyList
+last'' xs = xs !! ((length xs) - 1)
+
+canNotCallLastOnEmptyList = error "Can not call 'last' on an empty list"

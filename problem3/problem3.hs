f2db9ea603fe47a5afc8bc4631e050855df814a8
Alex Denton <alex.denton.dev@gmail.com>
2017-10-29 16:41:48 -0400

    Added unit tests for problem 3

--- problem3/problem3.hs
+++ problem3/problem3.hs
@@ -1,9 +1,32 @@
-main = do
-    print $ elementAt [1,2,3] 2
-    print $ elementAt [1,2] 3
+import Test.HUnit
+import Control.Exception
+import Test.HUnit.Tools
 
-elementAt [] _ = error "out of range"
+main = do
+    runTestTT elementAtTests
+
+elementAt :: [a] -> Int -> a
+elementAt [] _ = errorWithoutStackTrace indexOutOfRange
 elementAt (x:_) 1 = x
 elementAt (_:xs) k
-    | k < 1 = error ""
+    | k < 1 = errorWithoutStackTrace indexOutOfRange
     | otherwise = elementAt xs (k-1)
+
+indexOutOfRange = "Index out of range"
+
+elementAtTests = test [
+    shouldGetElementAt,
+    shouldErrorWithEmptyList,
+    shouldErrorWithIndexLargerThanList,
+    shouldErrorWithNegativeIndex]
+
+shouldGetElementAt = TestCase $ elementAt [1,2,3] 2 @?= 2
+
+shouldErrorWithEmptyList = TestCase $ assertRaises "Empty list raises error"
+    (ErrorCall indexOutOfRange) $ evaluate $ elementAt ([] :: [Int]) 3
+
+shouldErrorWithIndexLargerThanList = TestCase $ assertRaises "Index larger than list raises error"
+    (ErrorCall indexOutOfRange) $ evaluate $ elementAt [1,2] 3
+
+shouldErrorWithNegativeIndex = TestCase $ assertRaises "Negative index raises error"
+    (ErrorCall indexOutOfRange) $ evaluate $ elementAt [1,2,3] (-2)

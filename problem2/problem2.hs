057345e7f3b81a7e60fc63533165e36a268dc7f4
Alex Denton <alex.denton.dev@gmail.com>
2017-10-29 15:04:44 -0400

    Added unit tests for problem 2

--- problem2/problem2.hs
+++ problem2/problem2.hs
@@ -1,8 +1,28 @@
+import Test.HUnit
+import Control.Exception
+import Test.HUnit.Tools
+
 main = do
-    print $ secondToLast [1,2,3,4,5]
+    runTestTT secondToLastTests
 
 secondToLast :: [a] -> a
-secondToLast [] = error "not enough elements"
-secondToLast [x] = error "not enough elements"
+secondToLast [] = errorWithoutStackTrace canNotCallSecondToLastOnEmptyList
+secondToLast [x] = errorWithoutStackTrace canNotCallSecondToLastOnOneElementList
 secondToLast [x,_] = x
 secondToLast (x:xs) = secondToLast xs
+
+canNotCallSecondToLastOnEmptyList = "Can not call 'secondToLast' on empty list"
+canNotCallSecondToLastOnOneElementList = "Can not call 'secondToLast' on list with one element"
+
+secondToLastTests = test [
+    shouldReturnSecondToLastElement,
+    shouldReturnErrorWithEmptyList,
+    shouldReturnErrorWithOneElementList]
+
+shouldReturnSecondToLastElement = TestCase $ secondToLast [1,2,3,4] @?= 3
+
+shouldReturnErrorWithEmptyList = TestCase $ assertRaises "Empty list raises an error" 
+    (ErrorCall canNotCallSecondToLastOnEmptyList) $ evaluate (secondToLast [] :: String)
+
+shouldReturnErrorWithOneElementList = TestCase $ assertRaises "One element list raises an error" 
+    (ErrorCall canNotCallSecondToLastOnOneElementList) $ evaluate (secondToLast [1])

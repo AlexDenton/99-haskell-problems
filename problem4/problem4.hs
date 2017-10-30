7e26e3abb91ce042d0ec2084d26d6e8292acddb5
Alex Denton <alex.denton.dev@gmail.com>
2017-10-30 19:38:32 -0400

    Added another implementation of problem 4

--- problem4/problem4.hs
+++ problem4/problem4.hs
@@ -1,6 +1,11 @@
 main = do
     print $ length' [1,2,3,4,5]
+    print $ length' []
+    print $ length'' [1,2,3]
 
 length' :: [a] -> Int
 length' [] = 0
 length' (x:xs) = 1 + length' xs
+
+length'' :: [a] -> Int
+length'' xs = foldr (\_ -> (+1)) 0 xs

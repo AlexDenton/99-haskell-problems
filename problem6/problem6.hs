0d0491751f3e1f7538fda0308957e3108b1edbc3
Alex Denton <alex.denton.dev@gmail.com>
2017-10-30 23:04:53 -0400

    Added a more fun implementation for problem 6

--- problem6/problem6.hs
+++ problem6/problem6.hs
@@ -1,13 +1,23 @@
 import Test.HUnit
+import Control.Applicative
 
 main = do
     runTestTT isPalindromeTests
 
 isPalindrome xs = xs == reverse xs
 
-isPalindromeTests = test [
-    shouldReturnTrueWithPalindrome,
-    shouldReturnFalseWithNonPalindrome]
+-- apply on functions creates a new function which takes a value
+-- (in this case the list), calls the second function (in this case reverse) with the value
+-- and then calls the first function with the same list. In effect, reverse the list and compare
+-- to the original list. This is maybe not the most expressive but is very fun!
+isPalindrome' :: (Eq a) => [a] -> Bool
+isPalindrome' = (==) <*> reverse 
 
-shouldReturnTrueWithPalindrome = TestCase $ isPalindrome "kayak" @?= True
-shouldReturnFalseWithNonPalindrome = TestCase $ isPalindrome "canoe" @?= False
+isPalindromeTests = test [
+    shouldReturnTrueWithPalindrome isPalindrome,
+    shouldReturnFalseWithNonPalindrome isPalindrome,
+    shouldReturnTrueWithPalindrome isPalindrome',
+    shouldReturnFalseWithNonPalindrome isPalindrome']
+
+shouldReturnTrueWithPalindrome f = TestCase $ f "kayak" @?= True
+shouldReturnFalseWithNonPalindrome f = TestCase $ f "canoe" @?= False
